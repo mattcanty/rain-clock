984b0cc0b67322e6bfebf37d9c31a2899656f8ed
Matthew Canty <matthewcanty@gmail.com>
2014-09-25 17:21:17 +0000

    Add a little message in there to say data is being requested
    Also put loading of Angular at the top so that user information is ready to go

--- angular/clock.js
+++ angular/clock.js
@@ -2,10 +2,13 @@ var forecast;
 
 function Clock($scope, forecastApi){
 
+  $scope.summary = "Requesting Forecast...";
+
   $scope.refreshForecast = function(){
     forecastApi.getForecast(51.4572,-0.1092)
       .then(function(data){
         forecast = data;
+        $scope.summary = data.currently.summary;
     });
   }
   
