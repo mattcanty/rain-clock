3ecbb02b07804c01aa9b4946e0675048f42b10f7
Matthew Canty <matthewcanty@gmail.com>
2014-09-25 09:35:37 +0000

    Fix tab spaces
    Was 4, now 2.

--- angular/clock.js
+++ angular/clock.js
@@ -1,13 +1,13 @@
 var forecast;
 
 function Clock($scope, forecastApi){
-    
-    $scope.refreshForecast = function(){
-        forecastApi.getForecast(51.4572,-0.1092)
-            .then(function(data){
-                forecast = data;
-            });
-    }
-    
-    $scope.refreshForecast();
+
+  $scope.refreshForecast = function(){
+    forecastApi.getForecast(51.4572,-0.1092)
+      .then(function(data){
+        forecast = data;
+    });
+  }
+  
+  $scope.refreshForecast();
 }
--- angular/services.js
+++ angular/services.js
@@ -1,36 +1,35 @@
 var services = angular.module('services', []);
- 
-services.factory('forecastApi', function($http, $q) {
-    return({
-        getForecast: getForecast,
-    });
-    
-    function getForecast(lat, long) {
-        
-        var url = 'https://api.forecast.io/forecast/' + forecast_api_key + '/' + lat + ',' + long;
-        
-        var request = $http({
-            method: "jsonp",
-            url: url,
-            params: {
-                callback: 'JSON_CALLBACK'
-            }
-        });
-        
-        return( request.then( handleSuccess, handleError ) );
-    }
-    
-    function handleError( response ) {
-        if (!angular.isObject( response.data ) ||
-            !response.data.message) {
-            return( $q.reject( "An unknown error occurred." ) );
-        }
 
-        return( $q.reject( response.data.message ) );
-    }
-    
-    function handleSuccess( response ) {
-        console.log(response);
-        return( response.data );
-    }
+services.factory('forecastApi', function($http, $q) {
+	return({
+		getForecast: getForecast,
+	});
+	
+	function getForecast(lat, long) {
+	
+		var url = 'https://api.forecast.io/forecast/' + forecast_api_key + '/' + lat + ',' + long;
+		
+		var request = $http({
+			method: "jsonp",
+			url: url,
+			params: {
+			callback: 'JSON_CALLBACK'
+			}
+		});
+		
+		return( request.then( handleSuccess, handleError ) );
+	}
+	
+	function handleError( response ) {
+		if (!angular.isObject( response.data ) ||
+				!response.data.message) {
+			return( $q.reject( "An unknown error occurred." ) );
+		}
+		
+		return( $q.reject( response.data.message ) );
+	}
+	
+	function handleSuccess( response ) {
+		return( response.data );
+	}
 });
