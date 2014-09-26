652f315c2d404730fe71d58db221bafe24b2fb40
Matthew Canty <matthewcanty@gmail.com>
2014-09-26 09:56:05 +0000

    Get location from users browser
    Source:
    http://www.w3schools.com/html/html5_geolocation.asp

--- angular/clock.js
+++ angular/clock.js
@@ -5,11 +5,36 @@ function Clock($scope, forecastApi){
   $scope.summary = "Requesting Forecast...";
 
   $scope.refreshForecast = function(){
-    forecastApi.getForecast(51.4572,-0.1092)
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getForecastData, showError);
+    } else {
+      $scope.summary = "Geolocation is not supported by this browser.";
+    }
+  }
+
+  function getForecastData(position){
+    forecastApi.getForecast(position.coords.latitude,position.coords.longitude)
       .then(function(data){
         forecast = data;
         $scope.summary = data.currently.summary;
     });
+  }
+  
+  function showError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        $scope.summary = "User denied the request for Geolocation.";
+        break;
+      case error.POSITION_UNAVAILABLE:
+        $scope.summary = "Location information is unavailable.";
+        break;
+      case error.TIMEOUT:
+        $scope.summary = "The request to get user location timed out.";
+        break;
+      case error.UNKNOWN_ERROR:
+        $scope.summary = "An unknown error occurred.";
+        break;
+    }
   }
   
   $scope.refreshForecast();
