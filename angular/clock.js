6785e21a218ad96a71c39ca09cd022de47a28e5d
Matthew Canty <matthewcanty@gmail.com>
2014-09-29 22:42:03 +0000

    Show users location with the weather summary
    User location reverse geocoded using Google's geocoding api.
    Last updated of forecast also shown

--- angular/clock.js
+++ angular/clock.js
@@ -3,6 +3,8 @@ var forecast;
 function Clock($scope, apis){
 
   $scope.summary = "Requesting Forecast...";
+  $scope.position = "Searching for position...";
+  $scope.lastUpdated = "Never";
 
   $scope.refreshForecast = function(){
     if (navigator.geolocation) {
@@ -13,11 +15,29 @@ function Clock($scope, apis){
   }
 
   function getForecastData(position){
-    apis.getForecast(position.coords.latitude,position.coords.longitude)
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    
+    apis.getForecast(latitude, longitude)
       .then(function(data){
         forecast = data;
         $scope.summary = data.currently.summary;
+        $scope.lastUpdated = new Date().toLocaleString();
       });
+    
+    var geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(latitude, longitude);
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          $scope.position = results[0].formatted_address;
+        } else {
+          alert('No results found');
+        }
+      } else {
+        alert('Geocoder failed due to: ' + status);
+      }
+    });
   }
   
   function showError(error) {
