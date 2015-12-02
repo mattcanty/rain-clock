7b64fe1592d4e1ba004aee8b37deef96a4233b2c
Matthew Canty <matt.canty@tandem.co.uk>
2015-12-02 14:34:26 +0000

    Use browserify and browserify-css to bundle js and css

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.debug('loading requires');
+
+require('./../styles/app.css');
+require('./../styles/material-icons.css');
+
+require('./../node_modules/material-design-lite/material.min.js');
+
+console.debug('loaded requires');
