7e6c103dc7d8ecf0bdff90dc1660ae288acff1c8
Matthew Canty <matt.canty@tandem.co.uk>
2015-12-02 22:45:45 +0000

    First commit of browserify and vue

--- js/app.js
+++ js/app.js
@@ -1,8 +1,43 @@
-console.debug('loading requires');
+var Vue = require('vue');
+Vue.use(require('vue-resource'));
 
-require('./../styles/app.css');
-require('./../styles/material-icons.css');
+var data = {
+  message: 'Locating you...',
+  latitude: undefined,
+  longitude: undefined
+}
 
-require('./../node_modules/material-design-lite/material.min.js');
+function parseGeocodeResult(result) {
+  console.debug(result)
 
-console.debug('loaded requires');
+  data.message = result[0].formatted_address
+}
+
+function geocodePosition(latitude, longitude) {
+  var requestUri = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude;
+  
+  var response = Vue.http.get(requestUri, function(data, status, request){
+    console.debug(data)
+    console.debug(status)
+    console.debug(request)
+
+    parseGeocodeResult(data.results)
+  })
+}
+
+function updatePosition(currentPosition) {
+  console.debug(currentPosition)
+
+  data.latitude = currentPosition.coords.latitude
+  data.longitude = currentPosition.coords.longitude
+
+  data.message = 'found you at ' + data.latitude + ' ' + data.longitude
+
+  geocodePosition(data.latitude, data.longitude)
+}
+
+new Vue({
+  el: '#location',
+  data: data,
+  created: navigator.geolocation.getCurrentPosition(updatePosition, console.error)
+})
---
+++ js/index.js
@@ -0,0 +1,6 @@
+require('./../styles/app.css')
+require('./../styles/material-icons.css')
+
+require('./../node_modules/material-design-lite/material.min.js')
+
+require('./app.js')
