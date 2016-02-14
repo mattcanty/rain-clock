c359e5dbd0df8144c97f0477fe3c6a318a2e819a
Matt Canty <matthewcanty@gmail.com>
2016-02-14 21:28:24 +0000

    Add NewRelic monitoring

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('newrelic');
 var url = require('url');
 var express = require('express');
 var Forecast = require('forecast.io');
@@ -8,22 +9,22 @@ var oneDay = 86400000;
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
 app.get('/forecast/:latlong', function(req, res){
-  
+
   var path = url.parse(req.url).pathname;
 
   var coords = path.split('/')[2].split(',');
-  
+
   var options = {
     APIKey: process.env.FORECAST_API_KEY,
     timeout: 1000,
     exclude: 'daily,flags,alerts'
   };
-  
-  var forecast = new Forecast(options);  
-  
+
+  var forecast = new Forecast(options);
+
   forecast.get(coords[0], coords[1], function (err, forecastRes, data) {
     if (err) throw err;
-    
+
     res.end(JSON.stringify(data));
   });
 });
