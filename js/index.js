7597a4fd3f16ee23fb746cc8394eb0c5e1fd27b2
Matthew Canty <matthewcanty@gmail.com>
2016-07-02 18:16:25 +0100

    Remove Material-Design-Lite

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
-// Load styles
-
-require('./../styles/roboto-font.css')
-require('./../styles/app.css')
-require('./../styles/material-icons.css')
-
 // Load 3rd party scripts
 
-require('./../node_modules/material-design-lite/material.min.js')
 require('processing-js')
 
 // Load app scripts
