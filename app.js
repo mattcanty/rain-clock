a75a0b7c46ad15d676e61d926e178bb9c7aa1fa8
Matthew Canty <matthewcanty@gmail.com>
2017-03-06 22:04:10 +0000

    Move forecast request to separate module

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const async = require('async')
 const twitter = require('twitter')
 const request = require('request')
 const dotenv = require('dotenv')
+var forecast = require('./forecast.js')
 const app = express()
 
 dotenv.load()
 
 app.use(express.static(__dirname + '/public'))
 
-var client = new twitter({
+/*var client = new twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-})
+})*/
 
 app.get('/forecast/:latlong', function(req, res){
 
@@ -25,32 +26,12 @@ app.get('/forecast/:latlong', function(req, res){
   var lat = coords[0]
   var long = coords[1]
 
-  var requestUrl= `https://api.darksky.net/forecast/${process.env.FORECAST_API_KEY}/${lat},${long}?exclude=currently,daily,alerts,flags`
+  forecast.get(lat, long, function(result){
+    res.end(JSON.stringify(result))
+  });
 
-  async.parallel({
-    one: function(callback){
-      request(requestUrl, function (error, response, body) {
-        if (error) throw error
 
-        callback(null, JSON.parse(body))
-      })
-    }
-  }, function (error, results){
-    if (error) throw error
-
-    if(results.one.minutely){
-      var responseObject = {
-        summary: results.one.minutely.summary,
-        data: results.one.minutely.data.slice(0,60)
-      }
-    } else {
-      var responseObject = {
-        summary: results.one.hourly.summary
-      }
-    }
-
-    res.end(JSON.stringify(responseObject))
-
+    /*
     var twitterStatusUpdate =  {
       status:results.one.minutely.summary,
       display_coordinates:true,
@@ -59,7 +40,7 @@ app.get('/forecast/:latlong', function(req, res){
     }
 
     client.post('statuses/update', twitterStatusUpdate)
-  })
+    */
 })
 
 app.listen(process.env.PORT || 3000, console.log('running'))
