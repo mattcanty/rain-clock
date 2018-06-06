05d133ffd303fec40e0ff9cafdd9fcdf347681ae
Matthew Canty <matthewcanty@gmail.com>
2018-06-06 22:16:31 +0100

    Deploy frmo CircleCI

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-(cd aws/cloudformation/templates; sam validate -t *.yaml)
+PKG_BUCKET=rainclock-management-rainclocklambdabucket-978qdlqdm11f
+STACK_NAME=rainclock
 
-
-# rainclock-management-rainclocklambdabucket-978qdlqdm11f
+(cd aws/cloudformation/templates;
+    mkdir -p .build
+    sam validate -t *.yaml
+    sam package --template-file rainclock.yaml --s3-bucket $PKG_BUCKET --output-template-file .build/rainclock.yaml
+    sam deploy --template-file .build/rainclock.yaml --stack-name $STACK_NAME \
+        --parameter-overrides \
+            DarkSkyApiKey=$DARKSKY_APIKEY \
+        --capabilities CAPABILITY_IAM \
+        --region eu-west-2
+)
