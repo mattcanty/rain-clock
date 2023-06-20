c6bc3a086202e4d45004bbba3371c89535581632
Matt Canty <matthewcanty@gmail.com>
2023-06-20 15:10:29 +0100

    Update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,11 +5,11 @@ import styles from './footer.module.scss';
 const Footer: React.FunctionComponent = () => {
     return (
         <footer className={styles.footer}>
-            <a href={process.env.GITHUB_REPO_URL} title="GitHub - weather-clock" target="_blank" rel="noopener">
-                Created by Matt Canty
-            </a>
-            <a href={process.env.PIRATE_WEATHER_URL} title="Powered by Dark Sky" target="_blank" rel="noopener">
-                Powered by Dark Sky
+            Created by <a href={process.env.GITHUB_REPO_URL} title="GitHub - weather-clock" target="_blank" rel="noopener">
+                Matt Canty
+            </a> with UI ❤️ from Andrew Debens
+            <a href={process.env.PIRATE_WEATHER_URL} title="Powered by Pirate Weather" target="_blank" rel="noopener">
+                Powered by Pirate Weather
             </a>
         </footer>
     );
