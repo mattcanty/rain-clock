3141c330f04c846774e8171047a10a6edae38112
Matt Canty <matthewcanty@gmail.com>
2023-06-22 15:13:04 +0100

    Undo removing hard code

--- src/forecast/hooks/use-forecast-query.ts
+++ src/forecast/hooks/use-forecast-query.ts
@@ -5,7 +5,7 @@ import { ForecastData } from '../model';
 
 const FORECAST_FETCHER = (position: GeolocationPosition) =>
     fetch(
-        `${process.env.WEATHER_API}?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`,
+        `https://rainclock.mattcanty.com/api/get-minute-forecast?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`,
     ).then(r => r.json());
 
 /* every minute */
