7e716f4b97c08c71c23f00ec3095df1a6fd8c597
Matt Canty <matthewcanty@gmail.com>
2023-06-22 16:11:41 +0100

    Put footer content in <p>

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,16 @@ import styles from './footer.module.scss';
 const Footer: React.FunctionComponent = () => {
     return (
         <footer className={styles.footer}>
-            Created by Matt Canty with UI ❤️ from Andrew Debens
+            <p>
+                Created by
+                <a href={process.env.GITHUB_REPO_URL} title="GitHub - Rain Clock" rel="noopener">
+                    Matt Canty
+                </a>
+                with UI ❤️ from
+                <a href="https://github.com/Debens" title="GitHub - Andrew Debens" rel="noopener">
+                    Andrew Debens
+                </a>
+            </p>
             <a href={process.env.PIRATE_WEATHER_URL} title="Powered by Pirate Weather" rel="noopener">
                 Powered by Pirate Weather
             </a>
