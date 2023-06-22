0e4ba6dcb31595c604189c970f4def28686424b0
Matt Canty <matthewcanty@gmail.com>
2023-06-22 12:29:09 +0100

    Returning JSON body

--- netlify/functions/get-minute-forecast/main.go
+++ netlify/functions/get-minute-forecast/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -11,14 +12,30 @@ import (
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	fmt.Println("This message will show up in the CLI console.")
 
+	msg := fmt.Sprintf("Hello %v %v", *person.FirstName, *person.LastName)
+	responseBody := ResponseBody{
+		Message: &msg,
+	}
+	jbytes, err := json.Marshal(responseBody)
+	jstr := string(jbytes)
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode:      200,
-		Headers:         map[string]string{"Content-Type": "text/plain"},
-		Body:            "Hello, world!",
+		Headers:         map[string]string{"Content-Type": "application/json"},
+		Body:            jstr,
 		IsBase64Encoded: false,
 	}, nil
 }
 
 func main() {
 	lambda.Start(handler)
+}
+
+type ResponseBody struct {
+    Message *string json:"message"
+}
+
+type Person struct {
+    FirstName *string json:"firstName"
+    LastName  *string json:"lastName"
 }
