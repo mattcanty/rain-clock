3d9bd38676f3602c21df3d68458dea5f81c324f0
Matt Canty <matthewcanty@gmail.com>
2023-06-22 16:06:18 +0100

    One sentence

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import styles from './footer.module.scss';
 const Footer: React.FunctionComponent = () => {
     return (
         <footer className={styles.footer}>
-            Created by&nbsp;<a href={process.env.GITHUB_REPO_URL} title="GitHub - weather-clock" rel="noopener">Matt Canty</a>&nbsp;with UI ❤️ from Andrew Debens
+            Created by Matt Canty with UI ❤️ from Andrew Debens
             <a href={process.env.PIRATE_WEATHER_URL} title="Powered by Pirate Weather" rel="noopener">
                 Powered by Pirate Weather
             </a>
