aa461d0113220ac61f29571a95f04ad8b6a2cc82
Matt Canty <matthewcanty@gmail.com>
2023-06-25 13:57:00 +0100

    Update WaterLevelFace.tsx

--- src/components/WaterLevelFace/WaterLevelFace.tsx
+++ src/components/WaterLevelFace/WaterLevelFace.tsx
@@ -27,7 +27,7 @@ const useIntensityScale = () => {
         () =>
             d3
                 .scaleLinear()
-                .range([5, 0])
+                .range([0, 3])
                 .domain([0, Math.max(1, max)]),
         [max],
     );
